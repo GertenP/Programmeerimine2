2ab13c617047c9cb46def92d59411d23d83e688e
GertenP <gerten.pilv@tptlive.ee>
2024-11-17 17:36:54 +0200

    Add data classes

---
+++ KooliProjekt/Data/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KooliProjekt.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+    }
+}
---
+++ KooliProjekt/Data/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace KooliProjekt.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; } // Võõrvõti, mis viitab kliendile
+
+        public string Status { get; set; } = "Pending"; // Vaikimisi väärtus
+
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>(); // Tellimuse üksused
+
+        [Required]
+        public DateTime OrderDate { get; set; } = DateTime.Now; // Väärtustatakse alati tänase kuupäevaga
+    }
+}

d9f5f275991c92b4050267653ce743fa9050314b
GertenP <gerten.pilv@tptlive.ee>
2024-11-29 00:19:08 +0200

    add product data validation requirements

--- KooliProjekt/Data/Product.cs
+++ KooliProjekt/Data/Product.cs
@@ -1,13 +1,20 @@
-﻿namespace KooliProjekt.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KooliProjekt.Data
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Tootenimi on kohustuslik")]
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Tootehind on kohustuslik")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Kategooria valimine on kohustuslik")]
         public int CategoryId { get; set; }
         // vaikimisi null
-        public int Discount { get; set; } = 0;
+        [Range(0, 100, ErrorMessage = "Soodustus peab olema 0-100% vahemikus")]
+        public int? Discount { get; set; }
     }
 }

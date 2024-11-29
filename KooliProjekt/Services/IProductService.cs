4b5df7a691b7e0261fdfbb1fb2be69a75979b108
GertenP <gerten.pilv@tptlive.ee>
2024-11-29 11:28:56 +0300

    Add ProductService with interface

---
+++ KooliProjekt/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using KooliProjekt.Data;
+
+namespace KooliProjekt.Services
+{
+    public interface IProductService
+    {
+        Task<PagedResult<Product>> List(int page, int pageSize);
+        Task<List<Category>> GetCategoriesAsync();
+        Task<List<Product>> GetProductsAsync();
+
+        Task<Product> Get(int id);
+        Task Save (Product product);
+        Task Delete (int id);
+    }
+}
---
+++ KooliProjekt/Services/ProductService.cs
@@ -0,0 +1,61 @@
+﻿using KooliProjekt.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KooliProjekt.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<PagedResult<Product>> List(int page, int pageSize)
+        {
+            return await _context.Products.GetPagedAsync(page, 5);
+        }
+
+        public async Task<Product> Get(int id)
+        {
+            return await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Save(Product product)
+        {
+            if (product.Id == 0)
+            {
+                _context.Add(product);
+            }
+            else
+            {
+                _context.Update(product);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int Id)
+        {
+            var product = await _context.Products.FindAsync(Id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
